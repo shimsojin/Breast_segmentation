8939cb640b5722cecdba9fd9e598cb0832fea6e3
shimsojin <45342890+shimsojin@users.noreply.github.com>
2023-07-13 16:48:21 +0200

    Update DICOM_to_numpy.py

--- DICOM_to_numpy.py
+++ DICOM_to_numpy.py
@@ -12,18 +12,9 @@ import numpy as np
 import itertools
 
 
-#%% input
-HD = 'D:'
-year = '19' # 18/19/20
-season = 'SS' # SS/FW
-nseason = 0
-num = '2' # 1/2
-
-dirs_sel = ['Stand-L','Stand-R']
-#dirs_sel = ['Stand-L','Stand-R','Stand-L-prior','Stand-R_prior']
-
-uidcsv = HD+year+'_uid_info.csv'
-fnames = ['season','dir_number','uid','dir_list','converted dir','length']
+#%% Input
+path_uids = 'D:/test_batch'
+output_path = path_uids # default
 
 #%%
 def load_scan(path):
@@ -49,19 +40,19 @@ def get_pixels_hu(scans):
     return np.array(image, dtype=np.int16)
 
 def img_process(img_org,R,C):
-    default = 3 # dummy slices due to reconstruction restriction
-    # remove the ring (return to air)
+    # dummy slices due to reconstruction restriction
+    dummy = 3 
+    # remove the cylinder: If you want to include the cylinder in the numpy images, this part has to be moved to segmentation script for cylinder segmentation.
     temp = img_org
     r = C/2-30;
     for i, j in itertools.product(range(0,R),range(0,C)): # nested loops
-    #for i, j in zip(range(x), range(y)): #simultaneous loops
         if r < np.sqrt(((i+1)-R/2)**2+((j+1)-C/2)**2):
                 temp[:,i,j] = -2**10    
-    # find the length(index) of the breast
+    # save slices only upto the length(index) of the breast
     ob = temp > -500
     ob = np.mean(ob,axis=(1,2))
     l = np.sum(ob!=0)
-    img=temp[default:l]    
+    img=temp[dummy:l]    
     return img
 
 def convert_DICOM_to_Numpy(dir_dicom): 
@@ -72,54 +63,36 @@ def convert_DICOM_to_Numpy(dir_dicom):
     npimg = img_process(imgorg,R,C)
     return npimg
 
-
 #%%
-dir_DICOM = 'DICOM_retrieve_20'+year+'_'+season+'_anony_'+num
-path_uids = os.path.join(HD,dir_DICOM)
+
 uids = os.listdir(path_uids)
 uids.sort()
 
-output_path = path_uids
-
 for uid in uids:
-    dirlist = [] # list of dir in the uid
-    blist = [] # converted
-    lengths = []
-    path_dirs = os.path.join(path_uids,uid)
-    dirs = os.listdir(path_dirs)
+    path_DICOM = os.path.join(path_uids,uid)
     
-    dlist = []
-    for d in dirs:
-        if os.path.isdir(os.path.join(path_dirs,d)):
-            dirlist.append(d)
-    
-    for n,d in enumerate(dirs_sel):
-        path_DICOM = os.path.join(path_dirs,d)
-        if os.path.isdir(path_DICOM):
-            blist.append(d)
-            output_npimg = output_path+str(year)+str(nseason)+str(uid)+str(n)+'.npy' 
+    if os.path.isdir(path_DICOM):
+        output_npimg = path_DICOM+'.npy'
+        
+        dicomimg = load_scan(path_DICOM)
+        img = get_pixels_hu(dicomimg)
+        R = img.shape[1]
+        C = img.shape[2]
+        
+        npimg = img_process(img,R,C)
+        np.save(output_npimg, npimg)
+        
+        # generate cylinder for future image conversion to DICOM
+        
+        if 'cylinder.npy' not in uids:
+            temp = np.zeros((R,C),dtype=bool)
             
-            npimg = convert_DICOM_to_Numpy(path_DICOM) # load DICOM and get numpy
-            L = npimg.shape[0]
-            lengths.append(L)
-            
-            np.save(output_npimg, npimg)
-    length = np.sum(lengths)
-                    
-    #%% CSV file save
-    
-    seperator = ','
-    dl = seperator.join(dirlist)
-    bl = seperator.join(blist)
-    
-    if not os.path.isfile(uidcsv):
-        f = open(uidcsv, 'w')
-        with f:       
-            writer = csv.DictWriter(f, fieldnames=fnames)    
-            writer.writeheader()    
-            
-    with open(uidcsv, 'a', newline='') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=fnames)
-        writer.writerow({fnames[0]:season, fnames[1]:num, fnames[2]:str(uid), 
-                         fnames[3]:dl, fnames[4]:bl, fnames[5]:length})
+            r = C/2-30;
+            for i, j in itertools.product(range(0,R),range(0,C)): # nested loops
+                if r < np.sqrt(((i+1)-R/2)**2+((j+1)-C/2)**2):
+                        temp[i,j] = True   
+                        
+            cylinder = temp&(img[3]>-500)    
+            np.save(path_uids+'/cylinder.npy',cylinder)
+        
 
