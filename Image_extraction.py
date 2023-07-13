ef2d59d98211b793384d35251d38e823a3e4575c
shimsojin <45342890+shimsojin@users.noreply.github.com>
2023-07-13 16:46:41 +0200

    Update Image_extraction.py

--- Image_extraction.py
+++ Image_extraction.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Sojin SHIM, M.Sc., University Hospital Zurich, 2019-2021
-Segmentation of the breast CT image in HU
+Sojin SHIM, Ph.D., Instituete of Diagnostic and Interventional Radiology, University Hospital Zurich, 2019-2023
+Generate 3 representative images of segmentation in 3 planes.
 
 Input segmentation code
 0: air / 1: fat / 2: glands / 3: skin / 4: muscle / 5: bone / 6: fold / 7: implant / 8: suspected region / 9: cylinder
@@ -19,16 +19,9 @@ from scipy import interpolate
 now = datetime.datetime.now()
 print('Run at ',now.year, now.month, now.day, now.hour, now.minute, now.second)
     
-HD_path = "D:"
-
-year = '18' # 18/19/20
-season = 'FW' # SS/FW
-#nseason = 0 # 0/1/3/4 : SS1/SS2/SS3/SS4
-num = 1 # 1/2
-
-input_path = HD_path+'/numpy_20'+year+'_'+season+'_'+str(num)
+input_path = "D:/test_batch"
 output_path = input_path
-output_glandularity = output_path + "_Glandularity_comparison.csv"
+output_glandularity = output_path + "/Glandularity.csv"
 
 #%%
 npimgs = os.listdir(input_path)
@@ -75,42 +68,15 @@ for pnpimg in npimgs:
         breast_tissue = Fat|Gland
         
         C = np.sum(breast_tissue)
-        C_Skin = np.sum(Skin)
-        C_Muscle = np.sum(Muscle)
-        C_Fold = np.sum(Fold)
         
         HU_tissue = img[breast_tissue]
         meanHU = np.mean(HU_tissue)
         HU_G = HUcalibration(meanHU)
         
-        HU_temp = img[breast_tissue|Skin]
-        meanHU = np.mean(HU_temp)
-        HU_G_Skin = HUcalibration(meanHU)
-        
         Seg_G = np.sum(Gland)/C
-        Seg_G_Skin = (np.sum(Gland)+C_Skin)/(C+C_Skin)
-        
-        HU_G_Muscle = 0
-        Seg_G_Muscle = 0
-        HU_G_Fold = 0
-        Seg_G_Fold = 0
-        
-        if C_Muscle:
-            HU_temp = img[(breast_tissue)|Muscle]
-            meanHU = np.mean(HU_temp)
-            HU_G_Muscle = HUcalibration(meanHU)
-            Seg_G_Muscle = (np.sum(Gland)+C_Muscle)/(C+C_Muscle)
-            
-        if C_Fold:    
-            HU_temp = img[(breast_tissue)|Fold]
-            meanHU = np.mean(HU_temp)
-            HU_G_Fold = HUcalibration(meanHU)
-            Seg_G_Fold = np.sum(Gland)/(C+C_Fold)
         
         # CSV file for time record.
-        fnames = ['id', 'HU_G', 'Seg_G', 'HU_G_w_Skin', 'Seg_G_w_Skin', 
-                  'HU_G_w_Muscle', 'Seg_G_w_Muscle', 'HU_G_w_Fold', 'Seg_G_w_Fold',
-                  'C_Breast','C_Skin','C_Muscle','C_Fold']
+        fnames = ['uid', 'HU_G', 'Seg_G']
             # first time only: header
         if not os.path.isfile(output_glandularity):
             f = open(output_glandularity, 'w')
@@ -120,12 +86,7 @@ for pnpimg in npimgs:
                 
         with open(output_glandularity, 'a', newline='') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=fnames)
-            writer.writerow({fnames[0]:uid, fnames[1]:"%.4f" %HU_G, fnames[2]:"%.4f" %Seg_G, 
-                             fnames[3]:"%.4f" %HU_G_Skin, fnames[4]:"%.4f" %Seg_G_Skin, 
-                             fnames[5]:"%.4f" %HU_G_Muscle, fnames[6]:"%.4f" %Seg_G_Muscle, 
-                             fnames[7]:"%.4f" %HU_G_Fold, fnames[8]:"%.4f" %Seg_G_Fold,
-                             fnames[9]:"%.0f" %C,fnames[10]:"%.0f" %C_Skin,
-                             fnames[11]:"%.0f" %C_Muscle,fnames[12]:"%.0f" %C_Fold,})
+            writer.writerow({fnames[0]:uid, fnames[1]:"%.4f" %HU_G, fnames[2]:"%.4f" %Seg_G})
         
         breast = mask!=0
         
@@ -139,18 +100,12 @@ for pnpimg in npimgs:
         
         # save the image
         s=0
-        pltimg.imsave(output_path+'/img_%d_c.png' % int(uid),img[s], cmap='gray',vmin=offset, vmax=thSi)
-#        pltimg.imsave(output_path+'/img_%d_c.tiff' % int(uid),img[s], cmap='gray',vmin=offset, vmax=thSi, dpi=600, format="tiff")
-        pltimg.imsave(output_path+'/img_%d_s.png' % int(uid),img[:,:,tip[1]], cmap='gray',vmin=offset, vmax=thSi)
-#        pltimg.imsave(output_path+'/img_%d_s.tiff' % int(uid),img[:,:,tip[1]], cmap='gray',vmin=offset, vmax=thSi, dpi=600, format="tiff")
-        pltimg.imsave(output_path+'/img_%d_t.png' % int(uid),img[:,tip[0],:], cmap='gray',vmin=offset, vmax=thSi)
-#        pltimg.imsave(output_path+'/img_%d_t.tiff' % int(uid),img[:,tip[0],:], cmap='gray',vmin=offset, vmax=thSi, dpi=600, format="tiff")
+        pltimg.imsave(output_path+'/img_c_'+uid+'.png', img[s], cmap='gray', vmin=offset, vmax=thSi)
+        pltimg.imsave(output_path+'/img_s_'+uid+'.png', img[:,:,tip[1]], cmap='gray',vmin=offset, vmax=thSi)
+        pltimg.imsave(output_path+'/img_t_'+uid+'.png', img[:,tip[0],:], cmap='gray',vmin=offset, vmax=thSi)
         
         # save the mask
         s=0
-        pltimg.imsave(output_path+'/auto_seg_%d_c.png' % int(uid),mask[s], cmap='gray',vmin=0, vmax=6)
-#        pltimg.imsave(output_path+'/auto_seg_%d_c.tiff' % int(uid),mask[s], cmap='gray',vmin=0, vmax=6, dpi=600, format="tiff")
-        pltimg.imsave(output_path+'/auto_seg_%d_s.png' % int(uid),mask[:,:,tip[1]], cmap='gray',vmin=0, vmax=6)
-#        pltimg.imsave(output_path+'/auto_seg_%d_s.tiff' % int(uid),mask[:,:,tip[1]], cmap='gray',vmin=0, vmax=6, dpi=600, format="tiff")
-        pltimg.imsave(output_path+'/auto_seg_%d_t.png' % int(uid),mask[:,tip[0],:], cmap='gray',vmin=0, vmax=6)
-#        pltimg.imsave(output_path+'/auto_seg_%d_t.tiff' % int(uid),mask[:,tip[0],:], cmap='gray',vmin=0, vmax=6, dpi=600, format="tiff")
+        pltimg.imsave(output_path+'/auto_seg_c_'+uid+'.png', mask[s], cmap='gray',vmin=0, vmax=6)
+        pltimg.imsave(output_path+'/auto_seg_s_'+uid+'.png', mask[:,:,tip[1]], cmap='gray',vmin=0, vmax=6)
+        pltimg.imsave(output_path+'/auto_seg_t_'+uid+'.png', mask[:,tip[0],:], cmap='gray',vmin=0, vmax=6)
